76ecdf042c2fbc447975411cfb748f2791dc79a3
Josh Gibbs <upaymeifixit@gmail.com>
2018-07-31 23:13:56 -0700

    Needs async

--- index.js
+++ index.js
@@ -1,30 +1,33 @@
 const Nightmare = require('nightmare');
 require('nightmare-upload')(Nightmare);
-const nightmare = Nightmare({
-  show: true, 
-  width: 1100, 
-  height: 768
-});
+const nightmare = Nightmare({show: true, width: 1100, height: 768});
 const fs = require('fs');
 
-// Make sure we got a filename on the command line.
-if (process.argv.length < 5) {
-  console.log(`Usage: node ${process.argv[1]} USERNAME PASSWORD FILENAME`);
-  process.exit(1);
+init();
+
+async function init () {
+  // Make sure we got a filename on the command line.
+  if (process.argv.length < 5) {
+    console.log(`Usage: node ${process.argv[1]} USERNAME PASSWORD FILENAME`);
+    process.exit(1);
+  }
+
+  const USERNAME = process.argv[2];
+  const PASSWORD = process.argv[3];
+  const folder = process.argv[4];
+  const data = await fs.readFile(`${folder}/info.txt`);
+  const info = parseInfo(data);
+  postItems(info);
 }
 
-const USERNAME = process.argv[2];
-const PASSWORD = process.argv[3];
-const folder = process.argv[4];
-fs.readFile(`${folder}/info.txt`, 'utf8', parseInfo);
-
-let info = {description: ''};
-function parseInfo (err, data) {
+async function parseInfo (data) {
+  let info = {description: ''};
   const lines = data.split(/\r?\n/);
   for (i = 0; i < lines.length; i++) {
     switch (lines[i]) {
       case '{{PRICE}}': info.price = lines[++i]; break;
       case '{{TITLE}}': info.title = lines[++i]; break;
+      case '{{CATEGORY}}': info.category = lines[++i]; break;
       case '{{DESCRIPTION}}': 
         while (!lines[i + 1].match(/{{.*}}/)) {
           info.description += `${lines[++i]}\n`;
@@ -33,8 +36,11 @@ function parseInfo (err, data) {
       case '{{TAGS}}': info.tags = lines[++i]; break;
     }
   }
+  return info;
+}
 
-  nightmare
+async function postItems (info) {
+  await nightmare
     // Authenticate
     .goto('https://facebook.com/marketplace')
     .insert('#email', USERNAME)
@@ -51,23 +57,28 @@ function parseInfo (err, data) {
     .wait('div._4d0f._3-8_._4bl7') // "Item for Sale"
     .click('div._4d0f._3-8_._4bl7') // "Item for Sale"
 
-    // Fill out form data (way A)
-    // .wait(3000)
-    // .evaluate(() => {
-      // document.querySelector('input[placeholder="Select a Category"]').value = 'Antiques & Collectibles';
-      // document.querySelector('input[placeholder="What are you selling?"]').value = info.title;
-      // document.querySelector('input[placeholder="Price"]').value = 50;
-      // document.querySelector('div#placeholder-fv668').innerHTML = info.description;
-    // })  
+    // Fill out form data (Direct method)
+    .evaluate(() => {
+      document.querySelector('input[placeholder="Select a Category"]').value = info.category;
+      document.querySelector('input[placeholder="What are you selling?"]').value = info.title;
+      document.querySelector('input[placeholder="Price"]').value = info.price;
 
-    // Fill out form data (way B)
-    // .insert('input[placeholder="Select a Category"]', 'Antiques & Collectibles') // "Select a Category"
-    // .type('input[placeholder="What are you selling?"]', info.title)
-    // .type('input[placeholder="Price"]', info.price)
-    // .type('div#placeholder-fv668', info.description)
-    // .type('div[aria-describedby="placeholder-fv668"]', info.description)
+      for (element of document.querySelectorAll('div[id^="placeholder-"]')) {
+        if (element.innerHTML == 'Describe your item (optional)') {
+          element.innerHTML = info.description;
+          return;
+        }
+      }
+    })  
 
-    .upload('#js_1sd', `${folder}/photos/IMG_20171007_155145.jpg`)
+    // // Fill out form data (Nightmare method)
+    // // Setting the category this way doesn't work
+    // .type('input[placeholder="Select a Category"]', info.category) // "Select a Category"
+    // .insert('input[placeholder="What are you selling?"]', info.title) // "What are you selling?"
+    // .insert('input[placeholder="Price"]', info.price) // "Price"
+    // .click('div[data-testid="status-attachment-mentions-input"]') // "Describe your item (optional)"
+    // .insert('body', info.description)
+    // .upload('input[accept="image/*"]', `${folder}/photos/example_image_2.jpg`)
 
     // .end()
     .then(console.log)
