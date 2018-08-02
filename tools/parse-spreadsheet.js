79c4812ce7d3501f120dcb5d5ddc384a5dad0898
Josh Gibbs <upaymeifixit@gmail.com>
2018-08-01 18:39:30 -0700

    Updated spreadsheet parser

--- tools/parse-spreadsheet.js
+++ tools/parse-spreadsheet.js
@@ -6,39 +6,25 @@ async function init () {
     console.log(`Usage: node ${process.argv[1]} SPREADSHEET`);
     process.exit(1);
   }
-  const item_info = [{}];
-  const path = process.argv[2];
-  const tsv = await fs_extra.readFile(path, 'utf8');
-  const item_line = tsv.split(/\r?\n/);
-  for (let i = 1; i < 3; i++) {
-    const parts = item_line[i].split(/\t/);
-    item_info[i] = {
-      file: parts[0],
-      price: parts[1],
-      title: parts[2],
-      description: parts[3],
-      tags: parts[4],
-      category: parts[5]
+  const file_path = process.argv[2];
+  const tsv = await fs_extra.readFile(file_path, 'utf8');
+  const tsv_lines = tsv.split(/\r?\n/);
+  tsv_lines.shift(); // Remove header
+
+  const items = [{}];
+  for (let i = 0; i < 3; i++) {
+    const item_properties = tsv_lines[i].split(/\t/);
+    items[i] = {
+      file: item_properties[0],
+      price: item_properties[1],
+      title: item_properties[2],
+      description: item_properties[3].replaceAll('  ', '\n\n').replaceAll('\n ', '\n'),
+      tags: item_properties[4],
+      category: item_properties[5]
     };
   }
 
-  console.log(item_info);
-
-  // for (let i = 1; i < items.length; i++) {
-  //   const data = await fs_extra.readFile(`${folder}/${items[i]}/info.txt`, 'utf8');
-  //   console.log(await parseInfo(items[i], data));
-  // }
-}
-
-async function parseInfo (item, data) {
-  const lines = data.split(/\r?\n/);
-  let description = '';
-  for (let i = 5; lines[i] != '{{tags}}'; i++) {
-    description += lines[i] + '\n';
-  }
-  description = description.trim().replaceAll('\n', 'char(10)');
-
-  return `${item}; ${lines[1]}; ${lines[3]}; ${description}; ${lines[lines.length - 2]}`;
+  console.log(items);
 }
 
 String.prototype.replaceAll = function(search, replacement) {
