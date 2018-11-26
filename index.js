75684201b32a7ae2220265b79095a2e9185e7960
Josh Gibbs <upaymeifixit@gmail.com>
2018-11-26 13:06:47 -0800

    Fixed bug where no description would cause a crash

--- index.js
+++ index.js
@@ -8,6 +8,7 @@ async function init () {
   // const json = `{"price":10, "title": "Indecor Bathroom Sink Set", "description": "Comes with soap dispenser, toothbrush holder, and misc jar for toothpaste or other toiletries", "tags": "home decor decorate bathroom brown ceramic set upaymeifixit"}`;
   const json = await prompt('Enter JSON for sale item: ');
   const item = JSON.parse(json);
+  item.description = item.description || 'Please contact for more details.';
   listItem(item);
 }
 
@@ -16,16 +17,16 @@ async function listItem (item) {
   write('Opening Google Chrome. Tab order should be Craigslist, Facebook Marketplace, Letgo\n');
   await exec(`open -a Google\\ Chrome && exit`, execoutput);
 
-  // write('Creating Craigslist Ad...');
-  // await exec(`cliclick kd:cmd kp:num-1 ku:cmd kp:tab t:'${item.title}' kp:tab t:${item.price} kp:tab t:'${process.env.CL_SPECIFIC_LOCATION}' kp:tab t:'${process.env.CL_POSTAL_CODE}' kp:tab t:'${item.description}' kp:return,return t:'  tags: ${item.tags}' kp:tab,tab,tab,tab t:'${process.env.CL_PHONE_NUMBER}' kp:tab,tab t:'${process.env.CL_CONTACT_NAME}' kp:tab t:'${process.env.CL_STREET}' kp:tab t:'${process.env.CL_CROSS_STREET}' kp:tab t:'${process.env.CL_CITY}' kp:tab,space,tab,tab,tab,tab,tab,tab,arrow-down,arrow-down,arrow-down,tab,space,tab,space,tab,tab,tab,space,tab,space,return && sleep 3000 && exit`, execoutput);
-  // write('Done!\n');
+  write('Creating Craigslist Ad...');
+  await exec(`cliclick kd:cmd kp:num-1 ku:cmd kp:tab t:'${item.title}' kp:tab t:${item.price} kp:tab t:'${process.env.CL_SPECIFIC_LOCATION}' kp:tab t:'${process.env.CL_POSTAL_CODE}' kp:tab t:'${item.description}' kp:return,return t:'  tags: ${item.tags}' kp:tab,tab,tab,tab t:'${process.env.CL_PHONE_NUMBER}' kp:tab,tab t:'${process.env.CL_CONTACT_NAME}' kp:tab t:'${process.env.CL_STREET}' kp:tab t:'${process.env.CL_CROSS_STREET}' kp:tab t:'${process.env.CL_CITY}' kp:tab,space,tab,tab,tab,tab,tab,tab,arrow-down,arrow-down,arrow-down,tab,space,tab,space,tab,tab,tab,space,tab,space,return && sleep 3 && exit`, execoutput);
+  write('Done!\n');
 
-  // write('Creating Facebook Marketplace Ad...');
-  // await exec(`cliclick kd:cmd kp:num-2 ku:cmd kp:tab,tab,tab,tab,tab t:'${item.title}' kp:tab t:'${item.price}' kp:tab,tab,tab t:'${item.description}' kp:return,return t:'  tags: ${item.tags}' && sleep 3000 && exit`);
-  // write('Done!\n');
+  write('Creating Facebook Marketplace Ad...');
+  await exec(`cliclick kd:cmd kp:num-2 ku:cmd kp:tab,tab,tab,tab,tab t:'${item.title}' kp:tab t:'${item.price}' kp:tab,tab,tab t:'${item.description}' kp:return,return t:'  tags: ${item.tags}' && sleep 3 && exit`);
+  write('Done!\n');
 
   write('Creating Letgo Ad...');
-  await exec(`cliclick kd:cmd kp:num-3 ku:cmd kp:tab t:'${item.title}' kp:tab t:'${item.description}' kp:return,return t:'  tags: ${item.tags}' kp:tab t:'${item.price}' && sleep 3000 && exit`);
+  await exec(`cliclick kd:cmd kp:num-3 ku:cmd kp:tab t:'${item.title}' kp:tab t:'${item.description}' kp:return,return t:'  tags: ${item.tags}' kp:tab t:'${item.price}' && sleep 3 && exit`);
   write('Done!\n');
 }
 
